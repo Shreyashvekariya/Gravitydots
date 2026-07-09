7cd8581e9188f36e3fb58226afda897bad045fb6
shreyas_023 <shreyasvekaria2108@gmail.com>
2026-07-09 13:55:52 +0530

    Update Marquee and Services components

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -2,28 +2,28 @@ import './Marquee.css'
 
 function Marquee() {
   return (
-    <div className="marquee-section">
-      <div className="marquee-track">
-        <span>MORE LEADS</span>
-        <span className="star">✦</span>
-        <span>MORE SALES</span>
-        <span className="star">✦</span>
-        <span>MORE GROWTH</span>
-        <span className="star">✦</span>
-        <span>MORE LEADS</span>
-        <span className="star">✦</span>
+      <div className="marquee-section">
+        <div className="marquee-track">
+          <span>MORE LEADS</span>
+          <span className="star">✦</span>
+          <span>MORE SALES</span>
+          <span className="star">✦</span>
+          <span>MORE GROWTH</span>
+          <span className="star">✦</span>
+          <span>MORE LEADS</span>
+          <span className="star">✦</span>
+        </div>
+        <div className="marquee-track" aria-hidden="true">
+          <span>MORE SALES</span>
+          <span className="star">✦</span>
+          <span>MORE GROWTH</span>
+          <span className="star">✦</span>
+          <span>MORE LEADS</span>
+          <span className="star">✦</span>
+          <span>MORE SALES</span>
+          <span className="star">✦</span>
+        </div>
       </div>
-      <div className="marquee-track" aria-hidden="true">
-        <span>MORE SALES</span>
-        <span className="star">✦</span>
-        <span>MORE GROWTH</span>
-        <span className="star">✦</span>
-        <span>MORE LEADS</span>
-        <span className="star">✦</span>
-        <span>MORE SALES</span>
-        <span className="star">✦</span>
-      </div>
-    </div>
   )
 }
 
